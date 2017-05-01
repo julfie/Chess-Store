b8f06546802e38df938397bcc7dbbca4eaae3502
Juliann <jbfields@andrew.cmu.edu>
2017-05-01 01:47:47 -0400

    created page for 'how to play'

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   get 'about' => 'home#about', as: :about
   get 'contact' => 'home#contact', as: :contact
   get 'privacy' => 'home#privacy', as: :privacy
+  get 'howto' => 'home#howto', as: :howto
 
   get 'dashboard' => 'dashboard#dashboard', as: :dashboard
   
