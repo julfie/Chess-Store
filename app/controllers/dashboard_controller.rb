80ce57554a4b6dac43e43b11e4a0a18ab6e0646c
Juliann <jbfields@andrew.cmu.edu>
2017-04-30 20:56:34 -0400

    added login check

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,12 +2,14 @@ class DashboardController < ApplicationController
   # authorize_resource
 
   def dashboard
-  if (current_user.role? :shipper)
-    @unshipped_orders = Order.not_shipped.chronological
-    unless @unshipped_orders.empty? || @unshipped_orders.nil?
-    @order_items = Order.not_shipped.chronological.first.order_items
+    if logged_in?
+      if (current_user.role? :shipper)
+        @unshipped_orders = Order.not_shipped.chronological
+        unless @unshipped_orders.empty? || @unshipped_orders.nil?
+        @order_items = Order.not_shipped.chronological.first.order_items
+        end
+      end
     end
-  end
   end
 
   def ship
