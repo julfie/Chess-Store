020158c4563d171718ca4ee5f37c8e3651d5d402
Juliann <jbfields@andrew.cmu.edu>
2017-05-04 00:09:16 -0400

    basic item show page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,7 +21,7 @@ class ItemsController < ApplicationController
     # get the price history for this item
     @price_history = @item.item_prices.chronological.to_a
     # everyone sees similar items in the sidebar
-    @similar_items = Item.for_category(@item.category).active.alphabetical.to_a - [@item]
+    @similar_items = Item.for_category(@item.category).active.alphabetical.first(6).to_a - [@item]
   end
 
   def new
