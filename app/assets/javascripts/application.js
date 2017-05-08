02a1089c1924555f4a14edcea7dfc84fc9d900e7
Juliann <jbfields@andrew.cmu.edu>
2017-05-07 20:22:02 -0400

    form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,17 @@
 //= require jquery_ujs
 //= require foundation
 //= require_tree .
-//= require fusioncharts/fusioncharts
-//= require fusioncharts/fusioncharts.charts
-//= require fusioncharts/themes/fusioncharts.theme.fint
 
 $(function(){ $(document).foundation(); });
+
+$('.form-floating-label input, .form-floating-label textarea').focusin(function(){
+  $(this).parent().addClass('has-value');
+});
+
+$('.form-floating-label input, .form-floating-label textarea').blur(function(){
+  if(!$(this).val().length > 0) {
+    $(this).parent().removeClass('has-value');
+  }
+});
+
+
